24037ef01dc7600415b03d7d7e6a58a27d794278
Szymon Miękus <miekusoft@gmail.com>
2022-09-03 21:18:59 +0200

    Reload scene & particle effect

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,12 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float loadDelat = 1f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             Debug.Log("This the end");
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", loadDelat);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
